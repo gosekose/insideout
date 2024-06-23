0af34da3bd1b3ed74c76046057769a377c814f98
gosekose <ksyn1611@naver.com>
2024-06-23 13:56:02 +0900

    refactor: 도메인 / 데이터 모델 / 유즈케이스 개선 3 (ing) - aggregate 추가 - 데이터 모델 역할 축소 도메인 역할 분리

--- application/src/main/kotlin/com/insideout/usecase/feeling/RedefineFeelingsService.kt
+++ application/src/main/kotlin/com/insideout/usecase/feeling/RedefineFeelingsService.kt
@@ -4,10 +4,8 @@ import com.insideout.model.feeling.Feelings
 import com.insideout.usecase.feeling.port.FeelingReader
 import com.insideout.usecase.feeling.port.FeelingSaver
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional(readOnly = false)
 class RedefineFeelingsService(
     private val feelingSaver: FeelingSaver,
     private val feelingReader: FeelingReader,
